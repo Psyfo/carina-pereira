7f2276ff45f048b88f96d21d9c6d62d3437908a0
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-13 15:09:49 +0200

    Add Announcement component for displaying announcements

---
+++ src/app/components/Announcement/Announcement.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Announcement: React.FC = () => {
+  return (
+    <div className='flex align-center justify-center bg-cpOrange h-[30px] text-white'>
+      This is an announcement!
+    </div>
+  );
+};
+
+export default Announcement;

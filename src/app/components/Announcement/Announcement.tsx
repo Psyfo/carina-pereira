59667498b0ce3ed4a510f0b9d7ba6a0fa6836374
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-17 18:38:34 +0200

    Update Announcement component text and styling for clarity

--- src/app/components/Announcement/Announcement.tsx
+++ src/app/components/Announcement/Announcement.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <div className='flex align-center justify-center bg-cpOrange h-[30px] text-white'>
-      This is an announcement!
+    <div className='flex items-center justify-center bg-cpOrange h-[30px] text-white'>
+      <div className='font-inter text-white text-[13px]'>
+        enroll now for 2025
+      </div>
     </div>
   );
 };

b4b8486146c717c7b84222fb6c55f39f4a022ad1
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-17 18:38:47 +0200

    Enhance HeroSection with background image and logo for improved visual appeal

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -1,15 +1,24 @@
+import Image from 'next/image';
 import React from 'react';
 
 const HeroSection: React.FC = () => {
   return (
-    <div className='hero-section'>
-      <div className='hero-content'>
-        <h1 className='hero-title'>Welcome to Our Website</h1>
-        <p className='hero-subtitle'>
-          We are glad to have you here. Explore our content and enjoy your stay!
-        </p>
-        <button className='hero-button'>Get Started</button>
-      </div>
+    <div
+      className='hero-section w-full h-[70vh]'
+      style={{
+        backgroundImage: "url('/images/hero/hero.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Image
+        src='/images/hero/logo.svg'
+        alt='Hero Image'
+        width={100}
+        height={100}
+        unoptimized
+        className='w-[190px] absolute top-[1rem] left-[1rem]'
+      />
     </div>
   );
 };

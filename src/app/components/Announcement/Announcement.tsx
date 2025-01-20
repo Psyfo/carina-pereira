6d8223167a168bc2b05764159daad3d3982ab4d9
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-20 03:23:50 +0200

    Update Announcement component to position it at the top of the page for better visibility

--- src/app/components/Announcement/Announcement.tsx
+++ src/app/components/Announcement/Announcement.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <div className='flex items-center justify-center bg-cpOrange h-[30px] text-white'>
+    <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white'>
       <div className='font-inter text-white text-[13px]'>
         enroll now for 2025
       </div>

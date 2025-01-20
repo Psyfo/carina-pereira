fb597952393fa5848893a21afb75e962db60c29b
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-20 03:23:46 +0200

    Refactor HeroSection to improve responsiveness and update logo image for better visual consistency

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -3,21 +3,14 @@ import React from 'react';
 
 const HeroSection: React.FC = () => {
   return (
-    <div
-      className='hero-section w-full h-[70vh]'
-      style={{
-        backgroundImage: "url('/images/hero/hero.png')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+    <div className='hero-section w-full h-[80vh] md:h-[100vh] bg-[url("/images/hero/hero.png")] md:bg-[url("/images/hero/hero-md.png")] bg-cover bg-no-repeat bg-center'>
       <Image
-        src='/images/hero/logo.svg'
+        src='/images/hero/logo.png'
         alt='Hero Image'
         width={100}
         height={100}
         unoptimized
-        className='w-[190px] absolute top-[1rem] left-[1rem]'
+        className='w-[190px] lg:w-[284px] absolute top-[3rem] left-[1.5rem]'
       />
     </div>
   );

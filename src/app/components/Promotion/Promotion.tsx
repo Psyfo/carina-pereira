3ebb980aebc0d826a345f7e43e2b9ad265f9426a
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-24 17:05:59 +0200

    Add Promotion component to highlight enrollment benefits at Carina's International Makeup Academy

---
+++ src/app/components/Promotion/Promotion.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+/* eslint-disable react/no-unescaped-entities */
+
+const Promotion: React.FC = () => {
+  return (
+    <section className='h-[150vh] md:h-[100vh] md:flex'>
+      <div className='h-[50%] md:h-[100%] md:w-[50%] bg-[url("/images/promotion/promo.png")] md:bg-[url("/images/promotion/promo-md.png")] bg-center bg-cover'></div>
+      <div className='h-[50%] md:h-[100%] md:w-[50%] bg-cpOrange px-[30px] md:px-[100px] py-[75px] md:py-[155px] md:border-l-2 border-black'>
+        <div className='font-bold text-[29px] md:text-[35px] mt-auto'>
+          why enrol at Carina's International Makeup Academy?
+        </div>
+
+        <div className='font-inclusive text-[15px] md:text-[20px] leading-[150%] mt-8'>
+          <ul className='list-disc pl-8'>
+            <li>
+              Flexible Learning: Full-time, part-time, or self-paced options to
+              suit your schedule.
+            </li>
+            <li>
+              Professional Certification: Earn an internationally recognized
+              qualification.
+            </li>
+            <li>
+              Hands-On Experience: Real-world practice through photoshoots and
+              live projects.
+            </li>
+            <li>
+              Exclusive Career Opportunities: Access job placements with Carina
+              Pereira’s Pro Makeup Artist Agency.{' '}
+            </li>
+            <li>
+              Premium Tools & Support: Receive top-quality brushes, manuals, and
+              personalized kit consultations.
+            </li>
+          </ul>
+        </div>
+
+        <button className='inline-block bg-cpPink font-inclusive text-[16.5px] leading-[1.5] px-[35px] py-[8px] mb-[80px] border border-black rounded-full mt-12'>
+          enroll now
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Promotion;

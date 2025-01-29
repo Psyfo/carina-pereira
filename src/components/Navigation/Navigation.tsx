553de9c687d8304e6b3a5279545341709780f467
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-29 23:49:17 +0200

    Update Navigation component to use Next.js Link for routing and add z-index for improved layering

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navigation: React.FC = () => {
   return (
-    <nav className='items-center absolute top-[3rem] right-[1.5rem] hidden md:block'>
+    <nav className='items-center absolute top-[3rem] right-[1.5rem] hidden md:block z-20'>
       <ul className='flex gap-[35px] font-inter font-bold text-white text-[16px]'>
         <li>
-          <a href='#home'>Courses</a>
+          <Link href='/courses'>Courses</Link>
         </li>
         <li>
-          <a href='#about'>Services</a>
+          <Link href='/services'>Services</Link>
         </li>
         <li>
-          <a href='#contact'>Contact</a>
+          <Link href='/#contact'>Contact</Link>
         </li>
       </ul>
     </nav>

9826d3414b2b232375d45d75e1b3bc1ad7647064
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-29 23:48:14 +0200

    Add ExpressMakeupPage component for the Express Makeup Course

---
+++ src/app/courses/express-makeup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExpressMakeupPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Express Makeup Course</h1>
+      <p>Welcome to the Express Makeup Course page!</p>
+    </div>
+  );
+};
+
+export default ExpressMakeupPage;

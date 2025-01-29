1678256d97d3d500e217f2b0d88ca3d16c21d785
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-29 23:48:47 +0200

    Add HairstylingPage component for hairstyling courses

---
+++ src/app/courses/hairstyling/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HairstylingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hairstyling Courses</h1>
+      <p>
+        Welcome to the hairstyling courses page. Here you will find various
+        courses to enhance your hairstyling skills.
+      </p>
+    </div>
+  );
+};
+
+export default HairstylingPage;

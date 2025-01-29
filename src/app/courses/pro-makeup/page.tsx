519d0f80cec890d726193a93ceb45919c8eff8fc
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-29 23:48:52 +0200

    Add ProMakeupPage component for pro makeup courses

---
+++ src/app/courses/pro-makeup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProMakeupPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Pro Makeup Courses</h1>
+      <p>Welcome to the Pro Makeup Courses page!</p>
+    </div>
+  );
+};
+
+export default ProMakeupPage;

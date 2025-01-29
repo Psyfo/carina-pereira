2a510b8b5f2f2f6deb1ab14266693471e5187548
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-30 00:49:27 +0200

    Add Express Online Course page component

---
+++ src/app/courses/express-online/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExpressOnlinePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Express Online Course</h1>
+      <p>Welcome to the Express Online Course page!</p>
+    </div>
+  );
+};
+
+export default ExpressOnlinePage;

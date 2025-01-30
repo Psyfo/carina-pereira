abb2928ba29a33c23c57ab09a303b21f1d6c2dbf
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-30 13:46:13 +0200

    Refactor AboutSection component for improved layout and content presentation

--- src/app/courses/pro-makeup/components/AboutSection/AboutSection.tsx
+++ src/app/courses/pro-makeup/components/AboutSection/AboutSection.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 const AboutSection: React.FC = () => {
   return (
-    <section className='flex flex-col lg:flex-row lg:items-center lg:justify-center lg:gap-[125px] px-[25px] lg:px-[133px] py-[60px] lg:py-[120px]'>
-      <div className='lg:w-[600px]'>
-        <h1 className='font-tan-ashford font-bold tracking-wider'>
+    <section className='flex flex-col lg:flex-row md:items-center md:justify-between lg:gap-[125px] px-[25px] lg:px-[10%] py-[60px] lg:py-[120px]'>
+      <div className='lg:w-[600px] flex flex-col items-start'>
+        <h1 className='font-tan-ashford font-bold text-[19px] tracking-wider'>
           about this course:
         </h1>
 
@@ -30,18 +30,16 @@ const AboutSection: React.FC = () => {
         </p>
       </div>
 
-      <div className='lg:w-[550px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] mt-[84px] lg:mt-0'>
+      <div className='mx-auto lg:w-[45s0px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] mt-[84px] lg:mt-0'>
         <p className=''>Cost: R15 000</p>
-        <p className='font-inclusive text-[15px] leading-[1.5] mt-8'>
+        <p className=' mt-8'>
           Duration of this course: 2 months Full time & 4 months Part time
         </p>
-        <p className='font-inclusive text-[15px] leading-[1.5] mt-8'>
+        <p className=' mt-8'>
           Full time course: Tuesday to Friday 10:00am - 13:00pm
         </p>
-        <p className='font-inclusive text-[15px] leading-[1.5]'>
-          Part time course: Tuesday evenings 18:00pm - 21:00pm
-        </p>
-        <p className='font-inclusive text-[15px] leading-[1.5] mt-8'>
+        <p className=''>Part time course: Tuesday evenings 18:00pm - 21:00pm</p>
+        <p className=' mt-8'>
           Certificate & qualification: An internationally recognized certificate
           as well as work experience and assistance in job placements as well as
           the opportunity to assist Carina on brand shoots & weddings to gain

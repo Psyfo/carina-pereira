28ab4698bf07a700119448ccbcdba86221364b07
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-30 15:09:12 +0200

    Refactor Page component to include HeroSection, ServicesSection, and GallerySection for improved layout

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,14 @@
+import GallerySection from './components/GallerySection/GallerySection';
+import HeroSection from './components/HeroSection/HeroSection';
 import React from 'react';
+import ServicesSection from './components/ServicesSection/ServicesSection';
 
 const Page: React.FC = ({}) => {
   return (
     <div>
-      <h1>Services</h1>
-      <p>
-        Our services are designed to provide the best solutions for your needs.
-      </p>
+      <HeroSection />
+      <ServicesSection />
+      <GallerySection />
     </div>
   );
 };

274babd25f2e527bf2d62d98717e51a37d1a1f6b
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-30 15:09:07 +0200

    Add HeroSection component for enhanced landing page visuals

---
+++ src/app/services/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const HeroSection: React.FC = () => {
+  return (
+    <div className='relative hero-section w-full h-[80vh] md:h-[100vh] bg-[url("/images/services/hero/hero.png")] md:bg-[url("/images/services/hero/hero-md.png")] bg-cover bg-no-repeat bg-center'>
+      <Link href='/'>
+        <Image
+          src='/images/courses/hero/logo.png'
+          alt='Hero Image'
+          width={100}
+          height={100}
+          unoptimized
+          className='w-[190px] lg:w-[284px] absolute top-[3rem] left-[1.5rem]'
+        />
+      </Link>
+      <div className='absolute left-[40px] bottom-[70px] font-tan-ashford text-[40px] md:text-[47px] text-white tracking-wider lowercase'>
+        services
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;

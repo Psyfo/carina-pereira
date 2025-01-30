13ad06baee10e1eb46232a456a21a0622c24a4f2
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-30 13:46:17 +0200

    Add LearningSection component to Pro Makeup page with detailed course content

--- src/app/courses/pro-makeup/components/LearningSection/LearningSection.tsx
+++ src/app/courses/pro-makeup/components/LearningSection/LearningSection.tsx
@@ -1,7 +1,98 @@
 import React from 'react';
 
+/* eslint-disable react/no-unescaped-entities */
+
 const LearningSection: React.FC = () => {
-  return <div className='learning-section'></div>;
+  return (
+    <section className='px-[25px] lg:px-[10%] py-[33px]'>
+      <h1 className='font-tan-ashford font-bold text-[19px] tracking-wider'>
+        what you'll learn
+      </h1>
+
+      <div className='flex flex-col lg:flex-row items-center justify-center gap-[30px] mt-[30px]'>
+        <div className='w-full  flex flex-col gap-[30px]'>
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpPink'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              skincare
+            </h1>
+
+            <p className='mt-[40px]'>Identifying skin types</p>
+            <p className=''>Skincare and skincare steps</p>
+            <p className=''>Correct product choice and primers</p>
+            <p className=''>Guide to make-up brushes</p>
+          </div>
+
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpOrange'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              back to basics
+            </h1>
+
+            <p className='mt-[40px]'>
+              Application of blush, highlighter, bronzer and contour
+            </p>
+            <p className=''>Brow shaping</p>
+            <p className=''>
+              Eye chart and understanding eye colouring and shapes
+            </p>
+            <p className=''>Winged eyeliner</p>
+          </div>
+
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpPink'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              make-up looks
+            </h1>
+
+            <p className='mt-[40px]'>Bridal make-up</p>
+            <p className=''>Bridesmaid make-up</p>
+            <p className=''>Day wear make-up</p>
+            <p className=''>Monochromatic</p>
+            <p className=''>Perfect the red lip</p>
+            <p className=''>Mature skin make-up</p>
+          </div>
+        </div>
+
+        <div className='w-full  flex flex-col gap-[30px]'>
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpOrange'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              colour correction
+            </h1>
+
+            <p className='mt-[40px]'>
+              Full colour correcting and colour theory
+            </p>
+            <p className=''>Foundation matching and correct concealing</p>
+            <p className=''>Face shapes and how to enhance them</p>
+          </div>
+
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpPink'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              eye looks
+            </h1>
+
+            <p className='mt-[40px]'>Carbon smokey eye</p>
+            <p className=''>Bronzed smokey</p>
+            <p className=''>Halo eye</p>
+            <p className=''>Cut crease</p>
+            <p className=''>Half cut crease</p>
+            <p className=''>Winged shaped shadow</p>
+          </div>
+
+          <div className='h-[290px] font-inclusive text-[15px] leading-[1.5] border border-black rounded-2xl px-[20] py-[30] bg-cpOrange'>
+            <h1 className='font-tan-ashford font-bold text-[15px] tracking-wider'>
+              advanced make-up
+            </h1>
+
+            <p className='mt-[40px]'>Ethnic skin make-up</p>
+            <p className=''>False eyelash application</p>
+            <p className=''>Male grooming</p>
+            <p className=''>Creative (art) make-up</p>
+            <p className=''>SFX make-up</p>
+            <p className=''>Current make-up trend</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LearningSection;

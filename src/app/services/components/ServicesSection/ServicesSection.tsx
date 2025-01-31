3646b7338327ff5aa830b2c1ceae9e29e1fa4662
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-31 12:28:41 +0200

    Refactor ServicesSection layout for improved responsiveness and consistency

--- src/app/services/components/ServicesSection/ServicesSection.tsx
+++ src/app/services/components/ServicesSection/ServicesSection.tsx
@@ -10,24 +10,24 @@ const ServicesSection: React.FC = () => {
         carina's services
       </h1>
 
-      <div className='flex flex-col gap-[40px] mt-[30px]'>
-        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+      <div className='flex flex-col md:flex-row flex-wrap gap-[40px] md:gap-4 mt-[30px] lg:w-[900px]'>
+        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[15px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
           Bridal and bridal party make-up
         </div>
-        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[15px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
           One-one-One make-up lessons
         </div>
-        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[15px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
           Occasional make-up
         </div>
-        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[15px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
           Make-up workshops for parties
         </div>
-        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+        <div className='text-center font-inclusive text-[17.75px] leading-[1.5] px-[15px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
           Photoshoots/Editorial make-up
         </div>
         <Link href='#'>
-          <button className='w-full bg-cpPink font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4'>
+          <button className='w-full bg-cpPink font-inclusive text-[17.75px] leading-[1.5] px-[35px] py-[12px] border border-black rounded-full mt-4 md:mt-2'>
             book carina
           </button>
         </Link>

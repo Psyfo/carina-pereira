8f112ad6fe19adedafa78757606e41069854ebf5
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-31 15:03:04 +0200

    Refactor BrandSection to use BrandSlider component and implement autoplay feature for enhanced user experience

--- src/app/components/BrandSection/BrandSection.tsx
+++ src/app/components/BrandSection/BrandSection.tsx
@@ -1,46 +1,11 @@
 'use client';
-import Image from 'next/image';
+import BrandSlider from './components/BrandSlider';
 import React from 'react';
-
-//import BrandSlider from './components/BrandSlider';
 
 const BrandSection: React.FC = () => {
   return (
-    <section className='flex items-center justify-center h-[15vh] bg-cpPink overflow-x-auto px-8 lg:px-[15%]'>
-      <div className='flex gap-[65px] items-start lg:justify-center '>
-        <Image
-          src='/images/landing/brands/swiitch.png'
-          alt='Brand Image'
-          width={200}
-          height={100}
-          unoptimized
-          className=''
-        />
-        <Image
-          src='/images/landing/brands/gloei.png'
-          alt='Brand Image'
-          width={120}
-          height={100}
-          unoptimized
-          className=''
-        />
-        <Image
-          src='/images/landing/brands/mac.png'
-          alt='Brand Image'
-          width={150}
-          height={100}
-          unoptimized
-          className=''
-        />
-        <Image
-          src='/images/landing/brands/essence.png'
-          alt='Brand Image'
-          width={150}
-          height={100}
-          unoptimized
-          className=''
-        />
-      </div>
+    <section className='flex items-center justify-center h-[15vh] bg-cpPink px-8 lg:px-[15%]'>
+      <BrandSlider></BrandSlider>
     </section>
   );
 };
--- src/app/components/BrandSection/components/BrandSlider.tsx
+++ src/app/components/BrandSection/components/BrandSlider.tsx
@@ -3,27 +3,55 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Image from 'next/image';
 import React from 'react';
+import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const BrandSlider: React.FC = () => {
   const images = [
-    { src: '/images/brands/swiitch.svg', alt: 'Swiitch Beauty' },
-    { src: '/images/brands/gloei.svg', alt: 'Gloei' },
-    { src: '/images/brands/mac.svg', alt: 'MAC' },
-    { src: '/images/brands/essence.svg', alt: 'Essence' },
+    { src: '/images/landing/brands/swiitch.svg', alt: 'Swiitch Beauty' },
+    {
+      src: '/images/landing/brands/gloei.svg',
+      alt: 'Gloei',
+      className: 'py-8',
+    },
+    { src: '/images/landing/brands/mac.svg', alt: 'MAC' },
+    {
+      src: '/images/landing/brands/essence.svg',
+      alt: 'Essence',
+      className: 'py-8',
+    },
   ];
 
   return (
-    <Swiper spaceBetween={30} slidesPerView={3}>
+    <Swiper
+      slidesPerView={1}
+      modules={[Autoplay]}
+      loop
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+        waitForTransition: true,
+      }}
+      className='flex items-center justify-center w-full h-full'
+    >
       {images.map((image, index) => (
-        <SwiperSlide key={index}>
-          <Image
-            src={image.src}
-            alt={image.alt}
-            width={200}
-            height={100}
-            className='w-auto h-[100px]'
-          />
+        <SwiperSlide
+          key={index}
+          className='flex items-center justify-center h-[200px]'
+        >
+          <div className='flex items-center justify-center w-full h-full'>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={150}
+              height={200}
+              unoptimized
+              className={`object-contain h-full w-auto ${
+                image.className || ''
+              }`} // Apply conditional class
+            />
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>

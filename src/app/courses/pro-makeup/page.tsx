c1a587d009fcb41bdf98b1ba775338db562cbaf6
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-01-31 20:39:57 +0200

    Add 'use client' directive to pro-makeup page for client-side rendering

--- src/app/courses/pro-makeup/page.tsx
+++ src/app/courses/pro-makeup/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import AboutSection from './components/AboutSection/AboutSection';
 import HeroSection from './components/HeroSection/HeroSection';
 import LearningSection from './components/LearningSection/LearningSection';

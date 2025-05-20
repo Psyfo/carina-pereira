1596ac01dfd361b2d452c3a34c3d32506609609f
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-05-20 11:11:56 +0200

    feat: wrap announcement in Link component for navigation to courses

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white z-20'>
-      <div className='font-inter text-white text-[13px]'>
-        enroll now for 2025
-      </div>
-    </div>
+    <Link href='/courses' passHref>
+      <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white z-20'>
+        <div className='font-inter text-white text-[13px] cursor-pointer'>
+          enroll now for 2025
+        </div>
+      </div>{' '}
+    </Link>
   );
 };
 

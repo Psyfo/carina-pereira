269478a7d1f33c6dac61b157afe4b23d2d3b70d7
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-05-20 02:42:20 +0200

    fix: reorder imports for consistency and clarity in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-'use client';
-import About from './components/About/About';
-import AchievementsSection from './components/AchievementsSection/AchievementsSection';
-import Announcement from '../components/Announcement/Announcement';
-import BrandSection from './components/BrandSection/BrandSection';
-import Contact from './components/Contact/Contact';
-import CoursesSection from './components/CoursesSection/CoursesSection';
-import HeroSection from './components/HeroSection/HeroSection';
-import ImageFeed from './components/ImageFeed/ImageFeed';
-import OfferingsSection from './components/OfferingsSection/OfferingsSection';
-import Promotion from './components/Promotion/Promotion';
-import TestimonialSection from './components/TestimonialSection/TestimonialSection';
+"use client";
+import About from "./components/About/About";
+import AchievementsSection from "./components/AchievementsSection/AchievementsSection";
+import Announcement from "../components/Announcement/Announcement";
+import BrandSection from "./components/BrandSection/BrandSection";
+import Contact from "./components/Contact/Contact";
+import CoursesSection from "./components/CoursesSection/CoursesSection";
+import HeroSection from "./components/HeroSection/HeroSection";
+import ImageFeed from "./components/ImageFeed/ImageFeed";
+import MailingListModal from "@/components/MailingListModal/MailingListModal";
+import OfferingsSection from "./components/OfferingsSection/OfferingsSection";
+import Promotion from "./components/Promotion/Promotion";
+import TestimonialSection from "./components/TestimonialSection/TestimonialSection";
 
 export default function Home() {
   return (
     <div>
+      <MailingListModal />
       <Announcement />
       <HeroSection />
       <BrandSection />

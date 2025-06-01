4e1fd840103728bfa5ed1ed066d6d034fc4d32bd
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-06-01 22:17:27 +0200

    fix: remove unnecessary passHref prop from Link component in Announcement

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <Link href='/courses' passHref>
+    <Link href='/courses'>
       <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white z-20'>
         <div className='font-inter text-white text-[13px] cursor-pointer'>
           enroll now for 2025

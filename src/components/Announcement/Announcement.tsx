2c1b1ed3c1260d57f7da6ea65dec484e7d3227bf
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-20 20:08:54 +0200

    feat: update Announcement component link and text for women's day makeup masterclass

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <Link href='/courses'>
-      <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white z-20'>
+    <Link href='/courses/masterclass'>
+      <div className='flex absolute top-0 left-0 right-0 items-center justify-center bg-cpOrange h-[30px] text-white z-40'>
         <div className='font-inter text-white text-[13px] cursor-pointer'>
-          enroll now for 2025
+          get tickets for women’s day makeup masterclass
         </div>
       </div>{' '}
     </Link>

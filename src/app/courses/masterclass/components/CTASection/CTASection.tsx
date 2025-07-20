01da33e98fca59a0739c3e74d85adb788a7e1a38
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-20 20:08:51 +0200

    feat: add CTASection component for course call-to-action with animations

---
+++ src/app/courses/masterclass/components/CTASection/CTASection.tsx
@@ -0,0 +1,25 @@
+import CTA from '@/components/CTA/CTA';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const CTASection: React.FC = () => {
+  return (
+    <motion.section
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.3 }} // Trigger at 30% visibility
+      variants={{
+        visible: {
+          transition: {
+            staggerChildren: 0.2, // Stagger the children animations
+          },
+        },
+      }}
+      className='px-[25px] lg:px-[10%] py-[60px] lg:py-[120px]'
+    >
+      <CTA link='https://academy.carinapereira.com/product/pro-makeup-course/' />
+    </motion.section>
+  );
+};
+
+export default CTASection;

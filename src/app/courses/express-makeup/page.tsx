a707d2e5c9531715034cb86953e712d3b4a30ffa
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-24 00:19:40 +0200

    fix: add 'use client' directive and reorder import statements for consistency in ExpressMakeupPage

--- src/app/courses/express-makeup/page.tsx
+++ src/app/courses/express-makeup/page.tsx
@@ -1,9 +1,43 @@
-"use client";
-import AboutSection from "./components/AboutSection/AboutSection";
-import CTA from "./components/CTASection/CTASection";
-import HeroSection from "./components/HeroSection/HeroSection";
-import LearningSection from "./components/LearningSection/LearningSection";
-import React from "react";
+import React from 'react';
+
+import AboutSection from './components/AboutSection/AboutSection';
+import CTA from './components/CTASection/CTASection';
+import HeroSection from './components/HeroSection/HeroSection';
+import LearningSection from './components/LearningSection/LearningSection';
+
+export const metadata = {
+  title: 'Carina Pereira | Express Makeup Course',
+  description:
+    "Join Carina Pereira's Express Makeup Course to master professional makeup techniques. Enroll now for expert training.",
+  icons: '/images/favicon.svg',
+  keywords: [
+    'Carina Pereira',
+    'Express Makeup Course',
+    'Makeup Training',
+    'Professional Makeup',
+    'Makeup Techniques',
+  ],
+  authors: [{ name: 'Carina Pereira', url: 'https://carinapereira.com' }],
+  openGraph: {
+    title: 'Carina Pereira | Express Makeup Course',
+    description:
+      "Join Carina Pereira's Express Makeup Course to master professional makeup techniques. Enroll now for expert training.",
+    url: 'https://carinapereira.com/courses/express-makeup',
+    images: [
+      {
+        url: 'https://carinapereira.com/images/courses/express-makeup/hero.png',
+        width: 1200,
+        height: 630,
+        alt: 'Carina Pereira - Express Makeup Course',
+      },
+    ],
+  },
+  instagram: {
+    handle: '@carinapereirainternational',
+    site: 'https://www.instagram.com/carinapereirainternational/',
+    cardType: 'summary_large_image',
+  },
+};
 
 const ExpressMakeupPage: React.FC = () => {
   return (

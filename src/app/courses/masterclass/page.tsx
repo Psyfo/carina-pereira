b476dffd28623ab2c40cb982cb6af712dd4474c2
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-24 00:20:53 +0200

    fix: reorder import statements and add metadata for Masterclass page

--- src/app/courses/masterclass/page.tsx
+++ src/app/courses/masterclass/page.tsx
@@ -1,8 +1,34 @@
 'use client';
+import React from 'react';
+
 import AboutSection from './components/AboutSection/AboutSection';
 import CTA from './components/CTASection/CTASection';
 import HeroSection from './components/HeroSection/HeroSection';
-import React from 'react';
+
+// metadata for the Masterclass page
+export const metadata = {
+  title: 'Carina Pereira | Masterclass',
+  description:
+    'Join our Masterclass to elevate your makeup skills with expert guidance.',
+  keywords: ['makeup', 'masterclass', 'Carina Pereira', 'makeup course'],
+  authors: [{ name: 'Carina Pereira', url: 'https://carinapereira.com' }],
+  openGraph: {
+    title: 'Carina Pereira | Masterclass',
+    description: "Join our masterclass and celebrate Women's Day.",
+    url: 'https://carinapereira.com/masterclass',
+    images: [
+      {
+        url: 'https://carinapereira.com/images/courses/masterclass/hero.png',
+        alt: 'Masterclass Hero Image',
+      },
+    ],
+  },
+  instagram: {
+    handle: '@carinapereirainternational',
+    site: 'https://www.instagram.com/carinapereirainternational/',
+    cardType: 'summary_large_image',
+  },
+};
 
 const MasterclassPage: React.FC = () => {
   return (

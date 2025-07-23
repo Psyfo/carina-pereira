2df7da84c8c30a19c1a3e219d5db04f8b7aacd2e
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-24 00:18:07 +0200

    fix: remove unused metadata file from the project

--- src/app/metadata.ts
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: 'Carina Pereira',
-  description: 'Personal website of Carina Pereira',
-  icons: '/images/favicon.svg',
-};

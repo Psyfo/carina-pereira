cde5605c66782173a13472f603d850eaf0dbb4fb
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-27 21:17:19 +0200

    feat: add auth and utility functions for session management and class name merging

---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+'use server';
+
+import { getServerSession } from 'next-auth';
+
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+
+export async function getAuthSession() {
+  return await getServerSession(authOptions);
+}

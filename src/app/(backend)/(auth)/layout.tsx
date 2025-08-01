0f253161923ba2a4255eb90951c7a530ff6efde6
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-02 01:43:25 +0200

    feat: create AuthLayout component for authentication layout

---
+++ src/app/(backend)/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css';
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+      {children}
+    </div>
+  );
+}

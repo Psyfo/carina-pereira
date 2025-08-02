912ea880931c8a52c1c43ef3abde3e149cf851e9
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-02 22:44:04 +0200

    feat: add CoursesDashboardPage component for courses management

---
+++ src/app/(backend)/(admin)/dashboard/courses/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoursesDashboardPage: React.FC = () => {
+  return (
+    <main>
+      <h1>Courses Dashboard</h1>
+      {/* Add your courses management UI here */}
+    </main>
+  );
+};
+
+export default CoursesDashboardPage;

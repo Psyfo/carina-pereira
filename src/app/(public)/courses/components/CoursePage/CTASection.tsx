d0604e3df9e0ab020166a41c301ffc6418e34e9b
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-03 12:27:05 +0200

    feat: add CTASection component with animated display and customizable props

---
+++ src/app/(public)/courses/components/CoursePage/CTASection.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+'use client';
+
+import { motion } from 'framer-motion';
+import React from 'react';
+
+import CTA from '@/components/CTA/CTA';
+import { CTASectionData } from '@/types/CourseData';
+
+interface Props extends CTASectionData {}
+
+const CTASection: React.FC<Props> = ({
+  link,
+  heading,
+  text,
+  buttonLabel,
+  formType,
+}) => {
+  return (
+    <motion.section
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.3 }}
+      variants={{
+        visible: {
+          transition: {
+            staggerChildren: 0.2,
+          },
+        },
+      }}
+      className='px-[25px] lg:px-[10%] py-[60px] lg:py-[120px]'
+    >
+      <CTA
+        link={link ?? '#'}
+        heading={heading}
+        text={text}
+        buttonLabel={buttonLabel}
+        formType={formType}
+      />
+    </motion.section>
+  );
+};
+
+export default CTASection;

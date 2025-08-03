8057e67b886215ecfca894478a08964207cf92f3
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-03 22:47:27 +0200

    fix: update import path for HeroSectionData type

--- src/app/(public)/courses/components/CoursePage/HeroSection.tsx
+++ src/app/(public)/courses/components/CoursePage/HeroSection.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-import { HeroSectionData } from '@/types/CourseData';
+import { HeroSectionData } from '@/types/CoursePageProps';
 
 interface Props extends HeroSectionData {}
 

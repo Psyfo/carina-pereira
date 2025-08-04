affd6e39e68fd5a6e4472a1164ac292ef4a246b5
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-04 17:17:24 +0200

    feat: hash passwords when creating and updating users

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,12 @@
+import bcrypt from 'bcryptjs';
+
 import { connectDB } from '@/lib/db/mongodb';
 import { User } from '@/models/User';
 
 export async function createUser(email: string, password: string) {
   await connectDB();
-  const user = new User({ email, password });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ email, password: hashedPassword });
   return user.save();
 }
 
@@ -17,16 +20,25 @@ export async function getUserByEmail(email: string) {
   return User.findOne({ email });
 }
 
-export async function updateUser(id: string, data: Partial<typeof User>) {
+export async function updateUser(
+  id: string,
+  data: Partial<{ email: string; password: string }>
+) {
   await connectDB();
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10);
+  }
   return User.findByIdAndUpdate(id, data, { new: true });
 }
 
 export async function updateUserByEmail(
   email: string,
-  data: Partial<typeof User>
+  data: Partial<{ email: string; password: string }>
 ) {
   await connectDB();
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10);
+  }
   return User.findOneAndUpdate({ email }, data, { new: true });
 }
 
@@ -43,4 +55,14 @@ export async function deleteUserByEmail(email: string) {
 export async function getAllUsers() {
   await connectDB();
   return User.find({});
+}
+
+export async function changePassword(id: string, newPassword: string) {
+  await connectDB();
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  return User.findByIdAndUpdate(
+    id,
+    { password: hashedPassword },
+    { new: true }
+  );
 }
